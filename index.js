d008d4a1b13d840268dfc6325102b009891283da
Jamie Branch <schwung988@yahoo.com>
2023-09-13 00:21:07 -0400

    create functions and switch cases to shapes.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import inquirer from 'inquirer';
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {createShape} = require('./lib/shapes.js');
+
 
 inquirer.prompt([
     {
@@ -27,23 +29,38 @@ inquirer.prompt([
     {
         type: 'input',
         name: 'logoColor',
-        message: 'Select a logo color (#hexadecimal or keyword):',
-        validate: (input) => {
+        message: 'Select a shape color (#hexadecimal or keyword):',
+        
         //   // confirm if a CSS color
+        
         //   const isColor = coloursArray.includes(input.toLowerCase());
         //   // check if it is a valid hexadecimal color
         //   const isColorCode = /^#[0-9A-F]{6}$/i.test(input);
         //   return isColor || isColorCode;
-        },
+    
       },
 ]).then(answers => {
-    const shapesData = shapes(answers);
-    fs.writeFile('./examples/logo.svg', shapesData, err => {
-        if(err) {
-            console.log(err)
-        } else {
-            console.log('logo created');
-        }
-    })
+   const shape = createShape(answers);
+    console.log(shape);
+
+    
+    // function selection(filename, data) {
+    //     try {
+    //         fs.writeFile(path.join('/logo.svg', filename), data);
+    //         console.log("Created new LOGO!");
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    // fs.writeFile('logo.svg', shapesData, err => {
+    //     if(err) {
+    //         console.log(err)
+    //     } else {
+    //         console.log('logo created');
+    //     }
+    // })
     
 });
+
+
