32166d1ba9b3ffc2092c9271be1ae6ad8ee9d886
Jamie Branch <schwung988@yahoo.com>
2023-09-19 23:25:37 -0400

    logo.svg creation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {createShape} = require('./lib/shapes.js');
+const {Circle, Triangle, Square, Text} = require('./lib/shapes.js');
 
 
-inquirer.prompt([
+const promptUser = [
     {
         type: 'input',
         name: 'text',
-        message: 'Enter up to three characters:',
+        message: 'Enter one to three characters:',
         validate: (input) => input.length <= 3,
     },
     {
@@ -28,39 +28,104 @@ inquirer.prompt([
 
     {
         type: 'input',
-        name: 'logoColor',
-        message: 'Select a shape color (#hexadecimal or keyword):',
-        
-        //   // confirm if a CSS color
-        
-        //   const isColor = coloursArray.includes(input.toLowerCase());
-        //   // check if it is a valid hexadecimal color
-        //   const isColorCode = /^#[0-9A-F]{6}$/i.test(input);
-        //   return isColor || isColorCode;
-    
+        name: 'shapeColor',
+        message: 'Select a shape color (#hexadecimal or keyword):',    
       },
-]).then(answers => {
-   const shape = createShape(answers);
-    console.log('shape: ', shape);
+];
 
-    
-    // function selection(filename, data) {
-    //     try {
-    //         fs.writeFile(path.join('/logo.svg', filename), data);
-    //         console.log("Created new LOGO!");
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+//Create new logo in .svg file
+const writeLogo = (str) => {
+    fs.writeFile('logos.svg', str, (err) => {
+        if(err) {
+            console.log(err);
+        }else {
+            console.log('Success logo created!');
+        }
+    })
+}
 
-    // fs.writeFile('logo.svg', shapesData, err => {
-    //     if(err) {
-    //         console.log(err)
-    //     } else {
-    //         console.log('logo created');
-    //     }
-    // })
-    
-});
+class logoCreation {
+    constructor() {
+        this.userText = "",
+        this.userShape = ""
+    };
+    render() {
+        return `<svg
+        viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg">
+        ${this.userShape} ${this.userText}
+        </svg>`
+    };
+      
+    newShape(shape, text) {
+        this.userShape = shape.render();
+        this.userText = text.renderText();
+    };
+};
+
+//Initializes the logo design and file
+async function alpha() {
+    let createLogo = "";
+    let logoFile = "logos.svg";
+
+    const answers = await inquirer.prompt(promptUser);
+
+//Confirm shape selection
+
+shape = answers.shape;
+
+let shapeChoice;
+if (shape === "circle") {
+    shapeChoice = new Circle();
+    console.log("Shape selection is Circle");
+}
+else if (shape === "triangle") {
+    shapeChoice = new Triangle();
+    console.log("Shape selected is Triangle");
+}
+else if (shape === "square") {
+    shapeChoice = new Square();
+    console.log("Shape selected is Square");
+}
+else {
+    return "No shape selected, try again."
+};
+
+//Confirm shape color selection
+shapeColorChoice = answers.shapeColor;
+// console.log(`Shape color: ${shapeColorChoice}`);
+shapeChoice.setColor(shapeColorChoice);
+
+//Confirm text selection
+let textChoice = "";
+let textColorChoice = "";
+if(answers.text.length > 0 && answers.text.length < 4) {
+    textChoice = answers.text;
+} else {
+    console.log("Please select 1-3 letters");
+    return;
+}
+
+//Confirm text color
+textColorChoice = answers.textColor;
+const newText = new Text('', textChoice, textColorChoice);
+
+
+//Imports text and shape selections to the .svg file  
+const newLogo = new logoCreation();
+
+newLogo.newShape(shapeChoice, newText);
+
+const svgString = newLogo.render();
+
+
+// createLogo = newLogo.render();
+
+writeLogo(svgString);
+}
+
+alpha();
+
+
+
 
 
